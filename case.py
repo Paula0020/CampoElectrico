9da2cb6f6ca5a39fe08c294e1931e2ef3bf43397
Paula0020 <mpaula0020@hotmail.com>
2020-08-20 13:24:49 -0600

    Update case.py

--- case.py
+++ case.py
@@ -10,8 +10,10 @@ import modulos as md
 
 raiz=Tk()
 #raiz.config(bg=white)"
-raiz.geometry("800x600")
-raiz.title("Campo Eléctrico")
+raiz.geometry("700x400")
+raiz.title("Simulación de campo eléctrico 2020")
+#raiz.configure(width=200,height=100)
+#raiz.grid(padx=600, pady=600)
 
 
 md.bienvenida()
