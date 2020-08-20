de9ceb9550b322550e9b8b7a340f7e5ce5e1729d
Paula0020 <mpaula0020@hotmail.com>
2020-08-20 11:17:12 -0600

    Create modulos.py

---
+++ modulos.py
@@ -0,0 +1,55 @@
+"""
+María Paula Valdés
+Hector Aaron Pivaral
+Carné: 19146
+Carne:
+"""
+import tkinter as tk
+from tkinter import *
+"from tkinter import messagebox"
+
+
+def bienvenida():
+    Bienvenida = Label(text= "Bienvenido a calcular campos", font="Times 20")
+    Bienvenida.grid(row=0, column=100)
+
+def abrirventana():
+    label= tk.Label(text="Intesidad de campo", font= "Cambria 15")
+    label.grid(row=1,column=0)
+    entrada= tk.Entry(width=10, font= "Cambria 12")
+    entrada.grid(row=2,column=0)
+    
+    label2= tk.Label(text="Velocidad de particula", font= "Cambria 15")
+    label2.grid(row=1,column=100)
+    entrada2= tk.Entry(width=10, font= "Cambria 12")
+    entrada2.grid(row=2,column=100)
+
+def tipo():
+    label3= tk.Label(text="Tipo de particula", font= "Cambria 15")
+    label3.grid(row=1, column=150)
+     # electrón, positrón, protón, neutrón, partícula alfa, núcleo de deuterio, muón,
+    TIP= [
+        ("Proton","1"),
+        ("Electron","2"),
+        ("Neutron","3"),
+        ("Positrón","4"),
+        ("Partícula Alfa","5"),
+        ("Nucleo de Deuterio","6"),
+    ]
+    caracteristica= IntVar()
+    caracteristica.set("1")
+    x=2
+    for text, mode in TIP:
+        b= Radiobutton(text=text, variable=caracteristica, value=mode,font= "Cambria 12")
+        b.grid(row=x,column=150)
+        x=x+1
+    #label4= Label(text=caracteristica.get())
+    #label4.grid(row=2, column=200)
+
+def botonsimulacion():
+    aceptar = Button(text="Calcular", pady=10, font="Cambria 10")
+    aceptar.grid(row=15, column=680)
+    
+
+    
+    
