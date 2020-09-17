6491b9fa654bf87afc9e5c5df63114c5b5256d8b
Paula0020 <mpaula0020@hotmail.com>
2020-09-17 14:48:21 -0600

    Update ERROR.py

--- ERROR.py
+++ ERROR.py
@@ -4,10 +4,10 @@ Hector Aaron Pivaral
 Carné: 19146
 Carne: 19640
 """
-import tkinter as tk
+
 from tkinter import *
 from tkinter import messagebox
-root = Tk()
+
 
 velocidad=0
 Intensidad=0
@@ -21,26 +21,27 @@ def particulas(m,c):
     print(str(c))
     m= masa
     c=carga
-def validateV_float(var):
-    new_value = var.get()
+##velocidad, Intensidad
+def validateVI_float(V,I,elevadoI, elevadoV,ang):
+    new_value = V.get()
+    new_value2= I.get()
+    ElevaI= elevadoI.get()
+    ElevaV= elevadoV.get()
+    Al= ang.get()
     try:
         new_value == float(new_value)
-        velocidad = new_value
-        print(velocidad)
+        ElevaV == float(ElevaV)
+        velocidad = float(new_value)*(10**float(ElevaV))
+        new_value2== float(new_value2)
+        ElevaI == float(ElevaI)
+        Intensidad= float(new_value2)*(10**float(ElevaI))
+        Al == float(Al)
+        angulo= float(Al)
+        print(str(velocidad)+"\n"+str(Intensidad)+"\n"+str(angulo))
     except:
         messagebox.showinfo(title="Cuidado", message="Datos no válidos")
 
-def validateI_float(var):
-    new_value = var.get()
-    try:
-        new_value == float(new_value)
-        Intensidad = new_value
-        return(Intensidad)
-    except:
-        messagebox.showinfo(title="Cuidado", message="Datos no válidos")
-
-def Edata(IC,angle):
+def Edata(IC):
     verticalI=IC
-    angulo=angle
     
 
